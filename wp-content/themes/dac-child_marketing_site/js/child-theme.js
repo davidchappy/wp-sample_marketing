3cb3dcc6bb54e7e6d57944bfd6713fcba4a938ce
David Chapman <davidchappy@gmail.com>
2016-08-01 12:31:05 -0400

    Tooltips appear only once the window is ready

--- wp-content/themes/dac-child_marketing_site/js/child-theme.js
+++ wp-content/themes/dac-child_marketing_site/js/child-theme.js
@@ -5,25 +5,27 @@ jQuery(document).ready(function ( $ ) {
 	    scrollTop: $('#main-content').offset().top - $('.jumbotron').offset().top
   	}, 1000);
   });
-  $(function() {
-		$('.my-tooltip').hide();
-		$('.turn-on-tooltips').hide();
-	});
-	$(function() {
-		$('.my-tooltip').fadeIn(1000);
-	});
-  $(document).on('click', '.close-my-tooltip', function(event) {
-		$(this).parent().hide();
-		$('.turn-on-tooltips').show();
-  });
-  $(document).on('click', '.turn-off-tooltips', function(event) {
-		$('.my-tooltip').hide();
-		$('.turn-off-tooltips').hide();
-		$('.turn-on-tooltips').show();
-  });
-  $(document).on('click', '.turn-on-tooltips', function(event) {
-		$('.my-tooltip').show();
-		$('.turn-off-tooltips').show();
-		$('.turn-on-tooltips').hide();
+  $(window).ready(function($) {
+	  $(function() {
+			$('.my-tooltip').hide();
+			$('.turn-on-tooltips').hide();
+		});
+		$(function() {
+			$('.my-tooltip').fadeIn(1000);
+		});
+	  $(document).on('click', '.close-my-tooltip', function(event) {
+			$(this).parent().hide();
+			$('.turn-on-tooltips').show();
+	  });
+	  $(document).on('click', '.turn-off-tooltips', function(event) {
+			$('.my-tooltip').hide();
+			$('.turn-off-tooltips').hide();
+			$('.turn-on-tooltips').show();
+	  });
+	  $(document).on('click', '.turn-on-tooltips', function(event) {
+			$('.my-tooltip').show();
+			$('.turn-off-tooltips').show();
+			$('.turn-on-tooltips').hide();
+	  });
   });
 });
